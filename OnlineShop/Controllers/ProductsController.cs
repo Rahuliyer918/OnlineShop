3efbc83443717978b2b833724d28f27e8aed88cc
Rahul <rahuliyer918@gmail.com>
2025-05-01 22:29:10 -0400

    Completed Products Controller and Product details view

--- OnlineShop/Controllers/ProductsController.cs
+++ OnlineShop/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OnlineShop.Models.Db;
 
 namespace OnlineShop.Controllers
@@ -14,6 +15,26 @@ namespace OnlineShop.Controllers
         {
             List<Product> products = _context.Products.OrderByDescending(x => x.Id).ToList();
             return View(products);
+        }
+
+        public IActionResult Search(string searchText)
+        {
+            var products = _context.Products.Where(x => EF.Functions.Like(x.Title, "%" + searchText + "%") || EF.Functions.Like(x.Tags, "%" + searchText + "%")).OrderBy(x => x.Title).ToList();
+            return View("Index", products);
+        }
+
+        public IActionResult ProductDetails(int id)
+        {
+            Product? product = _context.Products.FirstOrDefault(x => x.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["gallery"] = _context.ProductsGalleries.Where(x => x.ProductId == id).ToList();
+
+            ViewData["NewProducts"] = _context.Products.Where(x => x.Id != id).Take(6).OrderByDescending(x => x.Id).ToList();
+            return View(product);
         }
     }
 }
